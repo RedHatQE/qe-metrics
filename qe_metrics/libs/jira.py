08284354594473f8623dccf9d3e163f659a92d8b
Caleb Evans <caevans@redhat.com>
2024-02-28 01:40:59 -0700

    [MSINTEROP-728] Start qe-metrics CLI (#13)
    * Add pyaml_env package
    
    * Start the cli entrypoint and add options
    
    * Start UserInput and Config objects
    
    * Start const.py file to define constants
    
    * Update COPY commands in Dockerfile. Copy in the qe_metrics folder and update the context of the file locations for pyproject.toml and poetry.lock

---
+++ qe_metrics/libs/jira.py
@@ -0,0 +1,20 @@
+from pyaml_env import parse_config
+from simple_logger.logger import get_logger
+
+
+class Jira:
+    def __init__(self, creds_file: str) -> None:
+        """
+        Initialize the Jira class
+
+        Args:
+            creds_file (str): Path to the yaml file holding database and Jira credentials.
+        """
+        self.logger = get_logger(name=self.__class__.__module__)
+
+        self.jira_creds = parse_config(creds_file)["jira"]
+
+        # TODO: Add Jira connection using config
+
+    # TODO: Add ability to execute Jira queries
+    # TODO: Validate Jira queries
