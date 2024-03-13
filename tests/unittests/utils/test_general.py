165df3b8036ee65c6681aebbcabfecf77dfad237
Caleb Evans <caevans@redhat.com>
2024-03-13 14:27:46 -0600

    Add check to verify_creds to ensure required key values are not None

--- tests/unittests/utils/test_general.py
+++ tests/unittests/utils/test_general.py
@@ -3,16 +3,27 @@ from qe_metrics.utils.general import verify_creds
 
 
 @pytest.fixture
-def creds():
+def valid_creds():
     return {"key1": "value1", "key2": "value2"}
 
 
-def test_verify_creds_with_all_required_keys_present(creds):
+@pytest.fixture
+def creds_none_value():
+    return {"key1": "value1", "key2": None}
+
+
+def test_verify_creds_with_all_required_keys_present(valid_creds):
     required_keys = ["key1", "key2"]
-    verify_creds(creds, required_keys)
+    verify_creds(valid_creds, required_keys)
 
 
-def test_verify_creds_with_missing_keys_raises_value_error(creds):
+def test_verify_creds_with_missing_keys_raises_value_error(valid_creds):
     required_keys = ["key1", "key2", "key3"]
     with pytest.raises(ValueError):
-        verify_creds(creds, required_keys)
+        verify_creds(valid_creds, required_keys)
+
+
+def test_verify_creds_with_none_values_raises_value_error(creds_none_value):
+    required_keys = ["key1", "key2"]
+    with pytest.raises(ValueError):
+        verify_creds(creds_none_value, required_keys)
