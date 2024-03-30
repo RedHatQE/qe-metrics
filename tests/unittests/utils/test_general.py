ece0908d0ee79f6bd394a1b4329dc9de6c330c28
Caleb Evans <caevans@redhat.com>
2024-03-30 09:00:39 -0600

    Add FCN plugin to flake8 (#67)
    * Add FCN to flake8
    
    * Make repository compliant to FCN flake8 plugin

--- tests/unittests/utils/test_general.py
+++ tests/unittests/utils/test_general.py
@@ -34,35 +34,35 @@ def queries_extra():
 
 def test_verify_config_with_all_required_keys_present(valid_config):
     required_keys = ["key1", "key2"]
-    verify_config(valid_config, required_keys)
+    verify_config(config=valid_config, required_keys=required_keys)
 
 
 def test_verify_config_with_missing_keys_raises_value_error(valid_config):
     required_keys = ["key1", "key2", "key3"]
     with pytest.raises(ValueError):
-        verify_config(valid_config, required_keys)
+        verify_config(config=valid_config, required_keys=required_keys)
 
 
 def test_verify_config_with_none_values_raises_value_error(config_none_value):
     required_keys = ["key1", "key2"]
     with pytest.raises(ValueError):
-        verify_config(config_none_value, required_keys)
+        verify_config(config=config_none_value, required_keys=required_keys)
 
 
 def test_verify_queries_with_all_required_queries_present(valid_queries):
-    verify_queries(valid_queries)
+    verify_queries(queries_dict=valid_queries)
 
 
 def test_verify_queries_with_missing_queries_raises_value_error(queries_missing_value):
     with pytest.raises(ValueError):
-        verify_queries(queries_missing_value)
+        verify_queries(queries_dict=queries_missing_value)
 
 
 def test_verify_queries_with_none_values_raises_value_error(queries_none_value):
     with pytest.raises(ValueError):
-        verify_queries(queries_none_value)
+        verify_queries(queries_dict=queries_none_value)
 
 
 def test_verify_queries_with_extra_queries_raises_value_error(queries_extra):
     with pytest.raises(ValueError):
-        verify_queries(queries_extra)
+        verify_queries(queries_dict=queries_extra)
