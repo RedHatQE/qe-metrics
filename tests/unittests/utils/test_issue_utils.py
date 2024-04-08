254e1e43afea06f65de5c9dff5e562ceb42d9e3c
Caleb Evans <caevans@redhat.com>
2024-04-08 10:47:13 -0600

    Add tests to support delete_old_issues and update some tests in test_issue_utils

--- tests/unittests/utils/test_issue_utils.py
+++ tests/unittests/utils/test_issue_utils.py
@@ -6,6 +6,7 @@ from qe_metrics.utils.issue_utils import (
     mark_obsolete_issues,
     create_update_issues,
     format_issue_date,
+    delete_old_issues,
 )
 
 
@@ -14,12 +15,7 @@ from qe_metrics.utils.issue_utils import (
     [
         pytest.param(
             ("update-existing-issue-product"),
-            [
-                {
-                    "key": "TEST-1234",
-                    "title": "New Test Summary",
-                }
-            ],
+            [{"key": "TEST-1234", "title": "New Test Summary", "last_updated": "2024-01-01T23:59:59.999999+0000"}],
             [
                 {
                     "issue_key": "TEST-1234",
@@ -29,8 +25,8 @@ from qe_metrics.utils.issue_utils import (
                     "severity": "blocker",
                     "status": "To Do",
                     "customer_escaped": True,
-                    "date_created": "2024-12-29",
-                    "last_updated": "2024-12-30",
+                    "date_created": "2023-12-29",
+                    "last_updated": "2023-12-30",
                 }
             ],
         ),
@@ -44,7 +40,7 @@ def test_update_existing_issue(product, raw_jira_issues, jira_issues):
         "severity": "critical",
         "status": "In Progress",
         "customer_escaped": False,
-        "last_updated": date(2023, 12, 31),
+        "last_updated": date(2024, 1, 1),
     }
     actual_values = {
         "title": jira_issues[0].title,
@@ -101,7 +97,9 @@ def test_update_existing_issue(product, raw_jira_issues, jira_issues):
 )
 def test_mark_obsolete_issues(product, raw_jira_issues, jira_issues):
     mark_obsolete_issues(current_issues=raw_jira_issues, db_issues=jira_issues, product=product)
-    assert JiraIssuesEntity.get(lambda issue: issue.issue_key == "TEST-1135").status == "obsolete"
+    assert (
+        JiraIssuesEntity.get(lambda issue: issue.issue_key == "TEST-1135").status == "obsolete"
+    ), f"Issue {jira_issues[1].issue_key} was not marked as obsolete."
 
 
 @pytest.mark.parametrize(
@@ -122,7 +120,9 @@ def test_mark_obsolete_issues(product, raw_jira_issues, jira_issues):
 )
 def test_create_update_issues_creates_issues(product, raw_jira_issues):
     create_update_issues(issues=raw_jira_issues, product=product, severity="blocker", jira_server="https://jira.com")
-    assert JiraIssuesEntity.get(issue_key="NEW-1234", product=product)
+    assert JiraIssuesEntity.get(
+        issue_key=raw_jira_issues[0].key, product=product
+    ), f"New issue {raw_jira_issues[0].key} was not created."
 
 
 @pytest.mark.parametrize(
@@ -143,7 +143,57 @@ def test_create_update_issues_creates_issues(product, raw_jira_issues):
 )
 def test_create_update_issues_none_bugs_not_created(product, raw_jira_issues):
     create_update_issues(issues=raw_jira_issues, product=product, severity="blocker", jira_server="https://jira.com")
-    assert not JiraIssuesEntity.get(issue_key="TASK-1234", product=product)
+    assert not JiraIssuesEntity.get(
+        issue_key=raw_jira_issues[0].key, product=product
+    ), f"Issue {raw_jira_issues[0].key} with issue type {raw_jira_issues[0].fields.issuetype.name} was created."
+
+
+@pytest.mark.parametrize(
+    "product, raw_jira_issues",
+    [
+        pytest.param(
+            ("create_update_issues_old_bugs_not_created_product"),
+            [
+                {"key": "OLD-1234", "title": "Test Summary", "last_updated": "2022-12-31T23:59:59.999999+0000"},
+            ],
+        ),
+    ],
+    indirect=True,
+)
+def test_create_update_issues_old_bugs_not_created(product, raw_jira_issues):
+    create_update_issues(issues=raw_jira_issues, product=product, severity="blocker", jira_server="https://jira.com")
+    assert not JiraIssuesEntity.get(
+        issue_key=raw_jira_issues[0].key, product=product
+    ), f"Old issue {raw_jira_issues[0].key} was created."
+
+
+@pytest.mark.parametrize(
+    "product, jira_issues",
+    [
+        pytest.param(
+            ("delete_old_issues_product"),
+            [
+                {
+                    "issue_key": "OLD-1234",
+                    "title": "Test Issue",
+                    "url": "https://jira.com",
+                    "project": "OLD",
+                    "severity": "blocker",
+                    "status": "Open",
+                    "customer_escaped": False,
+                    "date_created": "2023-01-01",
+                    "last_updated": "2023-01-01",
+                }
+            ],
+        ),
+    ],
+    indirect=True,
+)
+def test_delete_old_issues(product, jira_issues):
+    delete_old_issues()
+    assert not JiraIssuesEntity.get(
+        issue_key="OLD-1234", product=product
+    ), f"Old issue {jira_issues[0].issue_key} was not deleted."
 
 
 @pytest.mark.parametrize(
@@ -160,4 +210,6 @@ def test_create_update_issues_none_bugs_not_created(product, raw_jira_issues):
     indirect=True,
 )
 def test_format_issue_date(raw_jira_issues):
-    assert format_issue_date(raw_jira_issues[0].fields.updated) == date(2023, 1, 31)
+    assert format_issue_date(raw_jira_issues[0].fields.updated) == date(
+        2023, 1, 31
+    ), "Issue date not formatted properly."
