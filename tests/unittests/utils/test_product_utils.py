e8eb5527a85ad03434870d9f435dcbe85619e736
Caleb Evans <caevans@redhat.com>
2024-04-10 10:46:12 -0600

    linting

--- tests/unittests/utils/test_product_utils.py
+++ tests/unittests/utils/test_product_utils.py
@@ -15,10 +15,12 @@ def test_products_from_file(db_session, tmp_products_file):
         _product.name for _product in all_products
     ], "Test product test-from-file-product not found in database."
 
+
 def test_append_last_updated_arg_appends_arg():
     expected_query = 'project = TEST AND status = Open AND updated > "-90d"'
     query = append_last_updated_arg(query="project = TEST AND status = Open", look_back_days=90)
     assert query == expected_query, f"actual: {query} != expected: {expected_query}"
+
 
 def test_append_last_updated_arg_not_append_arg():
     expected_query = 'project = TEST AND status = Open AND updated > "-365d"'
