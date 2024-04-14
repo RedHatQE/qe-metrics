1ba34ff9030af4abc0fcef46ff0e85f3530e0497
Meni Yakove <441263+myakove@users.noreply.github.com>
2024-04-14 13:43:49 +0300

    Use `function_runner_with_pdb`, this will enter `pdb` when `--pdb` passed (#84)
    * Use `function_runner_with_pdb`, this will enter pdb if `--pdb` passed
    
    * Add missing `@orm.db_session`
    
    * Fix call `@orm.db_session`, remove deprecated test
    
    * fix merge conflicts

--- qe_metrics/cli.py
+++ qe_metrics/cli.py
@@ -1,17 +1,39 @@
 import os
-import sys
 
 import click
 from pony import orm
-from pyaml_env import parse_config
 
+from pyaml_env import parse_config
 from simple_logger.logger import get_logger
 from qe_metrics.libs.database import Database
 from qe_metrics.libs.jira import Jira
 from qe_metrics.utils.issue_utils import create_update_issues, delete_old_issues
-from qe_metrics.utils.product_utils import products_from_file, append_last_updated_arg
+from qe_metrics.utils.product_utils import append_last_updated_arg, products_from_file
+from pyhelper_utils.runners import function_runner_with_pdb
 
 LOGGER = get_logger(name="main-qe-metrics")
+
+
+def qe_metrics(products_file: str, config_file: str, verbose_db: bool) -> None:
+    """Gather QE Metrics"""
+    data_retention_days = parse_config(path=config_file)["database"].get("data_retention_days", 90)
+
+    with Database(config_file=config_file, verbose=verbose_db), Jira(config_file=config_file) as jira, orm.db_session:
+        for product_dict in products_from_file(products_file=products_file):
+            product, queries = product_dict.values()
+            for severity, query in queries.items():
+                LOGGER.info(f'Executing Jira query for "{product.name}" with severity "{severity}"')
+                try:
+                    if query := append_last_updated_arg(query=query, look_back_days=data_retention_days):
+                        create_update_issues(
+                            issues=jira.search(query=query),
+                            product=product,
+                            severity=severity,
+                            jira_server=jira.jira_config["server"],
+                        )
+                except Exception as ex:
+                    LOGGER.error(f'Failed to update issues for "{product.name}" with severity "{severity}": {ex}')
+        delete_old_issues(days_old=data_retention_days)
 
 
 @click.command()
@@ -38,46 +60,14 @@ LOGGER = get_logger(name="main-qe-metrics")
     help="Verbose output of database connection.",
     type=click.BOOL,
 )
-def main(products_file: str, config_file: str, pdb: bool, verbose_db: bool) -> None:
-    """Gather QE Metrics"""
-    data_retention_days = parse_config(path=config_file)["database"].get("data_retention_days", 90)
-
-    with Database(config_file=config_file, verbose=verbose_db), Jira(config_file=config_file) as jira, orm.db_session:
-        for product_dict in products_from_file(products_file=products_file):
-            product, queries = product_dict.values()
-            for severity, query in queries.items():
-                LOGGER.info(f'Executing Jira query for "{product.name}" with severity "{severity}"')
-                try:
-                    if query := append_last_updated_arg(query=query, look_back_days=data_retention_days):
-                        create_update_issues(
-                            issues=jira.search(query=query),
-                            product=product,
-                            severity=severity,
-                            jira_server=jira.jira_config["server"],
-                        )
-                except Exception as ex:
-                    LOGGER.error(f'Failed to update issues for "{product.name}" with severity "{severity}": {ex}')
-        delete_old_issues(days_old=data_retention_days)
+def cli_entrypoint(products_file: str, config_file: str, pdb: bool, verbose_db: bool) -> None:
+    function_runner_with_pdb(
+        func=qe_metrics,
+        products_file=products_file,
+        config_file=config_file,
+        verbose_db=verbose_db,
+    )
 
 
 if __name__ == "__main__":
-    should_raise = False
-    try:
-        main()
-    except Exception as ex:
-        import sys
-        import traceback
-
-        ipdb = __import__("ipdb")  # Bypass debug-statements pre-commit hook
-
-        if "--pdb" in sys.argv:
-            extype, value, tb = sys.exc_info()
-            traceback.print_exc()
-            ipdb.post_mortem(tb)
-        else:
-            LOGGER.error(ex)
-            should_raise = True
-
-    finally:
-        if should_raise:
-            sys.exit(1)
+    cli_entrypoint()
