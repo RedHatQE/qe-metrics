1fb71149eb8850823c22bea1eda6a022392518d9
Caleb Evans <caevans@redhat.com>
2024-04-15 11:31:24 -0600

    Modify tables to use sqlalchemy

--- qe_metrics/libs/database_mapping.py
+++ qe_metrics/libs/database_mapping.py
@@ -1,38 +1,41 @@
-from pony import orm
-from datetime import date
+from sqlalchemy import Integer, String, Boolean, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
+class Base(DeclarativeBase):
+    pass
 
-DB_OBJECT: orm.Database = orm.Database()
-
-
-class JiraIssuesEntity(DB_OBJECT.Entity):
-    """
-    A class to represent the JiraIssues table in the database.
-    """
-
-    _table_ = "jiraissues"
-
-    id = orm.PrimaryKey(int, auto=True)
-    product = orm.Required("ProductsEntity")
-    issue_key = orm.Required(str)
-    title = orm.Required(str)
-    url = orm.Required(str)
-    project = orm.Required(str)
-    severity = orm.Required(str)
-    status = orm.Required(str)
-    issue_type = orm.Required(str)
-    customer_escaped = orm.Required(bool)
-    date_created = orm.Required(date)
-    last_updated = orm.Required(date)
-
-
-class ProductsEntity(DB_OBJECT.Entity):
+class ProductsEntity(Base):
     """
     A class to represent the Products table in the database.
     """
 
-    _table_ = "products"
+    __tablename__ = "products"
 
-    id = orm.PrimaryKey(int, auto=True)
-    name = orm.Required(str, unique=True)
-    jira_issues = orm.Set("JiraIssuesEntity")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    jira_issues: Mapped["JiraIssuesEntity"] = relationship(back_populates="product")
+
+
+class JiraIssuesEntity(Base):
+    """
+    A class to represent the JiraIssues table in the database.
+    """
+
+    __tablename__ = "jiraissues"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    product_id: Mapped[int]= mapped_column(Integer, ForeignKey('products.id'))
+    product: Mapped["ProductsEntity"] = relationship(back_populates="jira_issues")
+    issue_key: Mapped[str] = mapped_column(String, nullable=False)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    project: Mapped[str] = mapped_column(String, nullable=False)
+    severity: Mapped[str] = mapped_column(String, nullable=False)
+    status: Mapped[str] = mapped_column(String, nullable=False)
+    issue_type: Mapped[str] = mapped_column(String, nullable=False)
+    customer_escaped: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    date_created: Mapped[Date] = mapped_column(Date, nullable=False)
+    last_updated: Mapped[Date] = mapped_column(Date, nullable=False)
